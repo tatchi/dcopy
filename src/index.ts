13bab20b2651e88d8886fb4be4c6727a18829672
tatchi <corentin.leruth@gmail.com>
2022-01-02 17:47:56 +0100

    fs: use sync version for compat with node < 14

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,23 @@
-import * as fs from "fs/promises";
+import * as fs from "fs";
+import { promisify } from "util";
 import * as path from "path";
 
+const readdir = promisify(fs.readdir);
+const mkdir = promisify(fs.mkdir);
+const copyFile = promisify(fs.copyFile);
+const stat = promisify(fs.stat);
+
 async function walker(src: string, dest: string, mode: number | undefined) {
-  let stats = await fs.stat(src);
+  let stats = await stat(src);
   if (stats.isDirectory()) {
-    await fs.mkdir(dest);
-    const files = await fs.readdir(src);
+    await mkdir(dest);
+    const files = await readdir(src);
     await Promise.all(
       files.map((name) => {
         return walker(path.join(src, name), path.join(dest, name), mode);
       })
     );
-  } else await fs.copyFile(src, dest, mode);
+  } else await copyFile(src, dest, mode);
 }
 
 export function dcopy(src: string, dest: string, mode?: number | undefined) {
